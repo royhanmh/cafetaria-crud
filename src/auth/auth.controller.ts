480bc0d1b607637d6b7ccd5aee6dbb3e5cf9bfd4
royhanmh <royhanm35@gmail.com>
2024-09-22 13:08:07 +0700

    configure swagger for auth resource

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Post, Body, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { ApiTags, ApiResponse, ApiBody } from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @ApiBody({ type: LoginDto }) // Documenting the request body
+  @ApiResponse({ status: 200, description: 'Login successful.' })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
   async login(@Body() loginDto: LoginDto) {
     const user = await this.authService.validateUser(
       loginDto.username,
--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,10 +1,13 @@
 import { IsString, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
+  @ApiProperty({ description: 'The username of the user' })
   @IsNotEmpty()
   @IsString()
   username: string;
 
+  @ApiProperty({ description: 'The password of the user' })
   @IsNotEmpty()
   @IsString()
   password: string;
